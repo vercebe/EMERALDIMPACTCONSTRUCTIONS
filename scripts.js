c636de813d3d80b8fffdf9f1938375bade2adf4c
vercebe <vercebe@hotmail.com>
2025-04-21 19:08:59 -0600

    hoy1

--- scripts.js
+++ scripts.js
@@ -8,6 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   const menuLinks = document.querySelectorAll(".navbar ul li a");
+
   menuLinks.forEach((link) => {
     link.addEventListener("click", function (e) {
       const targetHref = this.getAttribute("href");
@@ -15,6 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
       if (targetHref.startsWith("#")) {
         e.preventDefault();
         navLinks.classList.remove("show");
+
         const targetSection = document.querySelector(targetHref);
         if (targetSection) {
           window.scrollTo({
@@ -31,6 +33,7 @@ document.addEventListener("DOMContentLoaded", function () {
   document.addEventListener("click", function (event) {
     const isClickInsideMenu = navLinks.contains(event.target);
     const isClickOnMenuIcon = menuIcon.contains(event.target);
+
     if (!isClickInsideMenu && !isClickOnMenuIcon) {
       navLinks.classList.remove("show");
     }
@@ -39,6 +42,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const homeLink = document.getElementById("home-link");
   homeLink.addEventListener("click", function (event) {
     event.preventDefault();
+
     if (window.innerWidth <= 768) {
       document.getElementById("video-vertical").scrollIntoView({ behavior: "smooth" });
     } else {
@@ -66,50 +70,50 @@ document.addEventListener("DOMContentLoaded", function () {
   const sloganContainer = document.querySelector(".slogan-container");
   sloganContainer.style.display = "none";
 
-  // Mostrar slogan después de 3.5s si no hay transición de formulario
   setTimeout(function () {
     sloganContainer.style.display = "block";
     sloganContainer.classList.add("delayed-appearance");
   }, 3500);
 
   const parallaxContainer = document.getElementById("Conocenos");
+
   function applyParallaxEffect() {
     const scrolled = window.pageYOffset;
     const limit = parallaxContainer.offsetTop + parallaxContainer.offsetHeight;
+
     if (scrolled > parallaxContainer.offsetTop && scrolled <= limit) {
       const parallaxSpeed = 0.1;
-      parallaxContainer.style.backgroundPositionY = `${
-        (scrolled - parallaxContainer.offsetTop) * parallaxSpeed
-      }%`;
+      parallaxContainer.style.backgroundPositionY = `${(scrolled - parallaxContainer.offsetTop) * parallaxSpeed}%`;
     }
   }
+
   window.addEventListener("scroll", applyParallaxEffect);
 
-  // Email autocompletado
   const popularDomains = ["gmail.com", "hotmail.com", "yahoo.com", "outlook.com"];
   let isSuggesting = false;
+
   function applyEmailAutocomplete(emailInput) {
     emailInput.addEventListener("input", function (event) {
       const value = emailInput.value;
       const atIndex = value.indexOf("@");
+
       if (isSuggesting || event.inputType === "deleteContentBackward") {
         isSuggesting = false;
         return;
       }
+
       if (atIndex > -1) {
         const domainInput = value.slice(atIndex + 1);
-        const matchingDomain = popularDomains.find((domain) =>
-          domain.startsWith(domainInput)
-        );
+        const matchingDomain = popularDomains.find((domain) => domain.startsWith(domainInput));
+
         if (matchingDomain && domainInput.length > 0) {
           isSuggesting = true;
           const userInput = value.slice(0, atIndex + 1);
           emailInput.value = userInput + matchingDomain;
+
           emailInput.setAttribute("type", "text");
-          emailInput.setSelectionRange(
-            userInput.length + domainInput.length,
-            emailInput.value.length
-          );
+          emailInput.setSelectionRange(userInput.length + domainInput.length, emailInput.value.length);
+
           setTimeout(() => {
             isSuggesting = false;
             emailInput.setAttribute("type", "email");
@@ -122,13 +126,13 @@ document.addEventListener("DOMContentLoaded", function () {
       if (event.key === "Tab") {
         const value = emailInput.value;
         const atIndex = value.indexOf("@");
+
         if (atIndex > -1 && !value.slice(atIndex + 1).includes(".")) {
           event.preventDefault();
           const userInput = value.slice(0, atIndex);
           const domainInput = value.slice(atIndex + 1);
-          const matchingDomain = popularDomains.find((domain) =>
-            domain.startsWith(domainInput)
-          );
+          const matchingDomain = popularDomains.find((domain) => domain.startsWith(domainInput));
+
           if (matchingDomain) {
             emailInput.value = userInput + "@" + matchingDomain;
           }
@@ -137,12 +141,17 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
+  const emailInputPopup = document.getElementById("email");
   const emailInputContactanos = document.getElementById("correo");
+
+  if (emailInputPopup) {
+    applyEmailAutocomplete(emailInputPopup);
+  }
+
   if (emailInputContactanos) {
     applyEmailAutocomplete(emailInputContactanos);
   }
 
-  // Mostrar slogan en pantallas pequeñas después de 4 segundos
   setTimeout(function () {
     if (window.innerWidth <= 768) {
       const sloganContainers = document.querySelectorAll(".slogan-container");
@@ -151,4 +160,17 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     }
   }, 4000);
+});
+
+window.addEventListener("load", function () {
+  setTimeout(function () {
+    document.querySelector("header").classList.add("delayed-appearance");
+    document.querySelector("footer").classList.add("delayed-appearance");
+    document.querySelector(".whatsapp-tab").classList.add("delayed-appearance");
+
+    const formPopup = document.querySelector(".form-popup");
+    if (formPopup) {
+      formPopup.style.display = "none";
+    }
+  }, 3500);
 });
